44f6109b5c900f591c868046e301d919b5296d34
lemmebeHuh <sangkalaaji98@gmail.com>
2025-08-16 09:30:30 +0700

    revisi tampilan landing page

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12" x-data="{ editModal: false, transaction: {} }">
+    <div class="py-6" x-data="{ editModal: false, transaction: {} }">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             {{-- Notifikasi --}}
             @if (session('success'))
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -114,7 +114,7 @@
 
             <!-- Footer -->
             <footer class="bg-brand-dark">
-                <div class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+                <div class="mx-auto max-w-7xl px-6 py-12">
                     <div class="mt-8 md:order-1 md:mt-0">
                         <p class="text-center text-xs leading-5 text-gray-400">&copy; {{ date('Y') }} The Big Cheese. All rights reserved.</p>
                     </div>
